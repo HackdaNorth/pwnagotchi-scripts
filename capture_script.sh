243ae24d2d6174b9f7f2348ed1671d50565d41dd
Connor <36301205+HackdaNorth@users.noreply.github.com>
2024-05-28 16:34:58 -0400

    Update and rename capture script to capture_script.sh

--- capture_script.sh
+++ capture_script.sh
@@ -1,3 +1,3 @@
- sudo mkdir /home/pi/handshakes
+sudo mkdir /home/pi/handshakes
 root@Sandori:~# sudo cp ~/handshakes/* /home/pi/handshakes/
 scp -o IdentitiesOnly=yes -r -i ./id_ed25519 pi@10.0.0.2:/home/pi/handshakes/*  ./handshakes/pcap
